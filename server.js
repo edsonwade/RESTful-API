cede1a7b28fdbb73ea0dcf10a3bff731272e8ba0
edsonwade <vanilsonmuhongo@gmail.com>
2022-04-30 16:31:06 +0100

    crate aget route part 1

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const arr = require('./arrays.js')
+const arr = require("./arrays.js");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 let d = new Date(Date.UTC(2022, 04, 30));
 app.get("/", (req, res) => {
-    res
-        .send({
-            name: "vanilson",
-            project: "Restfull api with node + express",
-            date: d,
-        });
-    
+  res.send({
+    name: "vanilson",
+    project: "Restfull api with node + express",
+    date: d,
+  });
 });
 app.get("/api/v1/arrays", (req, res) => {
-    res
-        .send(arr);
-    
+  res.send(arr);
+});
+
+/* with parameters */
+app.get("/api/v1/lesson/:id", (req, res) => {
+  res.send(req.params.id);
+});
+/* Multiple params -> passing the year and name of the movie
+app.get("/api/v1/lesson/:year/:title", (req, res) => {
+  res.send(req.params);
+});*/
+
+/** query string params with question mark ? */
+app.get("/api/v1/lesson/:year/:title", (req, res) => {
+  res.send(req.query);
 });
 
 app.listen(PORT, () => {
